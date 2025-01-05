5a0d26087085d4e5f1fc3b14f659ab9c896bf8c0
Shaban Hassan <shabanhassan1010@gmail.com>
2025-01-05 12:36:59 +0200

    Renamed helloWorld.js to helloJavaScript.js, added printHelloGit, and created .gitignore

--- helloITI.js
+++ helloITI.js
@@ -1,4 +1,12 @@
-function printHelloWorld() {
+function printHelloWorld() 
+{
     console.log("Hello, ITI!");
 }
 
+function printNumbers() 
+{
+    let num1 = 9;
+    let num2 = 9;
+    let res  = num1 + num2;
+    console.log(res);
+}
--- helloWorld.js
+++
@@ -1,4 +0,0 @@
-function printHelloWorld() 
-{
-    console.log("Hello, World!");
-}
